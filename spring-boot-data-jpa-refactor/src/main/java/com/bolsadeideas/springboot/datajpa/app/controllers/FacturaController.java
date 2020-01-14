234815a775830a981b595ae47573072a91146a2b
i7 5820K <francofazzito7@gmail.com>
2020-01-14 13:03:52 -0300

    add ajax

--- spring-boot-data-jpa-refactor/src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/FacturaController.java
+++ spring-boot-data-jpa-refactor/src/main/java/com/bolsadeideas/springboot/datajpa/app/controllers/FacturaController.java
@@ -1,15 +1,16 @@
 package com.bolsadeideas.springboot.datajpa.app.controllers;
 
+
 import java.util.List;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,7 +24,7 @@ import com.bolsadeideas.springboot.datajpa.app.models.service.IclienteService;
 @RequestMapping("/factura")
 @SessionAttributes("factura")
 public class FacturaController {
-	private final org.slf4j.Logger LOG = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private IclienteService clienteService;
 	
@@ -48,12 +49,6 @@ public class FacturaController {
 	
 	@GetMapping(value = "/cargar-productos/{term}", produces = { "application/json" })
 	public @ResponseBody List<Producto> cargarProductos(@PathVariable String term) {
-		
-		LOG.debug("entrando consulta");
-		var lista = clienteService.findByNombre(term);
-		for (Producto producto : lista) {
-			LOG.debug(producto.getNombre());
-		}
-		return lista;
+		return clienteService.findByNombre(term);
 	}
 }
